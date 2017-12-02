1f8affcc03108c897d2a5a51a9409990adbdd78e
prasad <hpmtissera@gmail.com>
2017-12-02 17:54:10 +0900

    #1: Setup Gradle project

---
+++ src/main/java/com/lanka_guide/mynxttrip/MynxttripApplication.java
@@ -0,0 +1,12 @@
+package com.lanka_guide.mynxttrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynxttripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MynxttripApplication.class, args);
+	}
+}
